5b939abbb7396f66d6c328d753f5fb3e6e8041cd
cajahnke <cajahnke@users.noreply.github.com>
2018-05-09 07:48:02 -0500

    Update courses_controller.rb

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -550,8 +550,7 @@ class CoursesController < ApplicationController
       if params[:course][:repository_id].blank? || @course.repository_id == params[:course][:repository_id].to_i
         return false
       else
-        new_repo = Repository.find(params[:course][:repository_id].to_i)
-        return ! new_repo.affiliated?(current_user)
+        return true
       end
     end
 
